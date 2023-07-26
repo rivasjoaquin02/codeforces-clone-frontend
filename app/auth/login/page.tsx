fc71748d8dd240920ebab3fbbafe74ca7d1062a7
strange <rivasjoaquin02@gmail.com>
2023-07-25 22:27:28 -0400

    added signout and login pages

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/Forms/LoginForm";
+
+const LoginPage = () => {
+    return (
+        <main className="form-page">
+            <LoginForm />
+        </main>
+    );
+};
+
+export default LoginPage;
---
+++ app/auth/signout/page.tsx
@@ -0,0 +1,11 @@
+import SignOutForm from "@/components/Forms/SignOutForm";
+
+const SignOutPage = () => {
+    return (
+        <main className="form-page">
+            <SignOutForm />
+        </main>
+    );
+};
+
+export default SignOutPage;
