6a9a5c4fa7655e4a03ee084b56bd8d3e81b3b56b
strange <rivasjoaquin02@gmail.com>
2023-07-25 22:28:01 -0400

    added SignoutForm and LoginForm

---
+++ components/Forms/LoginForm.tsx
@@ -0,0 +1,24 @@
+const loginFields = [
+    { value: "username", label: "Username", type: "text", required: true },
+    { value: "password", label: "Password", type: "password", required: true },
+];
+
+const apiroute = "http://localhost:8000";
+
+const SignOutForm = () => {
+    return (
+        <form action={`${apiroute}/auth/signout`} method="POST">
+            {loginFields.map((field) => (
+                <>
+                    <label htmlFor={field.value}> {field.label}</label>
+                    <input type={field.type} required={field.required} />
+                </>
+            ))}
+            <button type="submit" className="btn btn-submit">
+                Sign out
+            </button>
+        </form>
+    );
+};
+
+export default SignOutForm;
