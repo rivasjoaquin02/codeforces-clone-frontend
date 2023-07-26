f54b9e3b0374bf674edf6fc41c35b6f94da165ee
strange <rivasjoaquin02@gmail.com>
2023-07-26 18:01:33 -0400

    added problems create page

--- components/Problems/ProblemsCreate.tsx
+++ components/Problems/ProblemsCreate.tsx
@@ -1,55 +1,194 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiZXhwIjoxNjkwNDEwMTM4fQ.0IbNMHLrOmg9bw9AqT4nsm42xKobLjhbrMzidd5ec34";
+
+interface SubmitProblem {
+    title: string;
+    description: string;
+    example_input: string;
+    example_output: string;
+    tags: Array<string>;
+    difficulty: string;
+}
+
+const API_URL = "http://127.0.0.1:8000";
+
 const ProblemsCreate = () => {
+    const [title, setTitle] = useState<string>();
+    const [difficulty, setDifficulty] = useState<string>("easy");
+    const [tags, setTags] = useState<Array<string>>();
+    const [inputExample, setInputExample] = useState<string>();
+    const [outputExample, setOutputExample] = useState<string>();
+    const [description, setDescription] = useState<string>();
+
+    const handleClick = () => {
+        if (
+            !title ||
+            !difficulty ||
+            !tags ||
+            !inputExample ||
+            !outputExample ||
+            !description
+        )
+            return;
+
+        const data = {
+            title,
+            description,
+            example_input: inputExample,
+            example_output: outputExample,
+            tags,
+            difficulty,
+        };
+
+        console.log(data);
+
+        axios
+            .post(`${API_URL}/problems/create`, data, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `bearer ${token}`,
+                },
+            })
+            .then((res) => console.log(res.data))
+            .catch((e) => console.log(e));
+    };
+
     return (
         <div className="problems">
-            <div className="__problems-problem-info">
-                {/* title difficulty */}
-                <div className="box border">
-                    <div className="__problems-title-difficulty">
-                        {/* Title */}
-                        <div>
-                            <label htmlFor="problem-title">
-                                <h3>Title</h3>
-                            </label>
-                            <input type="text" id="problem-title" />
-                        </div>
-                        {/* Difficulty */}
-                        <div>
-                            <label htmlFor="problem-difficulty">
-                                <h3>Difficulty</h3>
-                            </label>
-                            <div id="problem-difficulty">
-                                <input type="radio" value="easy" />
-                                <input type="radio" value="medium" />
-                                <input type="radio" value="hard" />
-                            </div>
-                        </div>
+            <div className="create-problems">
+                {/* title difficulty tags */}
+                <div
+                    className="problem-info box border"
+                    style={{ gridRow: "span 2" }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "5px",
+                        }}
+                    >
+                        <label htmlFor="title">
+                            <h3>Title</h3>
+                        </label>
+                        <input
+                            type="text"
+                            id="title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
                     </div>
-                    <div className="__problems-inout">
-                        {/* input example */}
+
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "5px",
+                        }}
+                    >
+                        <label htmlFor="difficulty">
+                            <h3>Difficulty</h3>
+                        </label>
+                        <select
+                            name="difficulty"
+                            id="difficulty"
+                            value={difficulty}
+                            onChange={(e) => setDifficulty(e.target.value)}
+                        >
+                            <option value="easy">Easy</option>
+                            <option value="medium">Medium</option>
+                            <option value="hard">Hard</option>
+                        </select>
+                    </div>
+
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "5px",
+                        }}
+                    >
+                        <label htmlFor="tags">
+                            <h3>Tags</h3>
+                        </label>
+                        <input
+                            type="text"
+                            id="tags"
+                            value={tags}
+                            onChange={(e) => setTags(e.target.value.split(","))}
+                        />
+                    </div>
+                </div>
+
+                {/* submit */}
+                <div className="box border" style={{ gridColumn: "span 2" }}>
+                    <button
+                        className="btn btn-primary border"
+                        onClick={handleClick}
+                    >
+                        Submit
+                    </button>
+                </div>
+
+                {/* description */}
+                <div
+                    className="box border"
+                    style={{ gridColumn: "span 2", gridRow: "span 3" }}
+                >
+                    <label htmlFor="description">
+                        <h3>Problem Description</h3>
+                    </label>
+                    <textarea
+                        name="description"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
+
+                {/* input output example */}
+                <div className="box border">
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "5px",
+                        }}
+                    >
                         <label htmlFor="input-example">
                             <h3>Input Example</h3>
                         </label>
-                        <input type="text" id="input-example" />
-                        {/* output example */}
+                        <input
+                            type="text"
+                            id="input-example"
+                            value={inputExample}
+                            onChange={(e) => setInputExample(e.target.value)}
+                        />
+                    </div>
+
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "5px",
+                        }}
+                    >
                         <label htmlFor="output-example">
                             <h3>Output Example</h3>
                         </label>
-                        <input type="text" id="output-example" />
+                        <input
+                            type="text"
+                            id="output-example"
+                            value={outputExample}
+                            onChange={(e) => setOutputExample(e.target.value)}
+                        />
                     </div>
                 </div>
-                {/* statement */}
-                <div className="box border">
-                    <label htmlFor="problem-statment">
-                        <h3>Problem Statement</h3>
-                    </label>
-                    <textarea
-                        name="problem-statment"
-                        id="problem-statment"
-                    ></textarea>
-                </div>
-            </div>
-            <div className="box border">
-                <button className="btn btn-primary">Submit</button>
             </div>
         </div>
     );
