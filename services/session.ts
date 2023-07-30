b4a0efe41b9dc7e4fa4733ea0dda1666d2779967
strange <rivasjoaquin02@gmail.com>
2023-07-30 13:33:47 -0400

    improvement of session with better error handling

--- services/session.ts
+++ services/session.ts
@@ -1,15 +1,50 @@
-import { Session } from "./login";
+import { Result } from "@/types";
 
-export const storeSession = (newSession: Session) => {
-    window.localStorage.setItem("session", JSON.stringify(newSession));
+export interface Session {
+    username: string;
+    access_token: string;
+    token_type: string;
+}
+
+const isSession = (obj: any): obj is Session => {
+    return (
+        obj &&
+        typeof obj.username === "string" &&
+        typeof obj.access_token === "string" &&
+        typeof obj.token_type === "string"
+    );
 };
 
-export const getSession = (): Session => {
-    const sessionJSON = window.localStorage.getItem("session");
+export const storeSession = (newSession: Session): Result<Session> => {
+    if (!newSession) return { success: false, error: "Invalid Session" };
+    if (!isSession(newSession))
+        return { success: false, error: "Missing Session Fields" };
 
-    if (!sessionJSON)
-        throw new Error("There is not session in the LocalStorage");
-        
-    const session = JSON.parse(sessionJSON);
-    return session;
+    try {
+        const sessionJSON = JSON.stringify(newSession);
+        window.localStorage.setItem("session", sessionJSON);
+    } catch (JSONError) {
+        return { success: false, error: "Failed to convert Session to json" };
+    }
+
+    return { success: true, data: newSession };
+};
+
+export const getSession = (): Result<Session> => {
+    try {
+        const sessionJSON = window.localStorage.getItem("session");
+        if (!sessionJSON)
+            return { success: false, error: "No Session in Local Storage" };
+
+        const session = JSON.parse(sessionJSON) as Session;
+        if (!isSession(session))
+            return {
+                success: false,
+                error: "Session in Local Storage does not has the Session Shape",
+            };
+
+        return { success: true, data: session };
+    } catch (JSONError) {
+        return { success: false, error: "Failed to convert Session from json" };
+    }
 };
