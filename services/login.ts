b7a9f35fe36bc1b59431563486700403619e2cdf
strange <rivasjoaquin02@gmail.com>
2023-07-30 14:26:15 -0400

    improvement of problem login service with better error handling

--- services/login.ts
+++ services/login.ts
@@ -1,28 +1,76 @@
+import { Result } from "@/types";
 import axios from "axios";
+import { Session, storeSession } from "./session";
 
 const baseUrl = "http://127.0.0.1:8000/auth/login";
 
-interface Login {
+interface LoginCredentials {
     username: string;
     password: string;
 }
 
-
-
-const login = async (credentials: Login): Promise<Session> => {
-    if (!credentials) throw new Error("Theres no Credentials");
-
-    const credentialsRecord: Record<string, string> = {
-        username: credentials.username,
-        password: credentials.password,
-    };
-
-    const { data: Session } = await axios.post(
-        baseUrl,
-        new URLSearchParams(credentialsRecord)
+const isLoginCredentials = (obj: any): obj is LoginCredentials => {
+    return (
+        obj &&
+        typeof obj.username === "string" &&
+        typeof obj.password === "string"
     );
+};
 
-    return { username: credentials.username, ...Session };
+type Token = {
+    access_token: string;
+    token_type: string;
+};
+
+const login = async (
+    credentials: LoginCredentials
+): Promise<Result<Session>> => {
+    if (!isLoginCredentials(credentials))
+        return {
+            success: false,
+            error: "Credentials in wrong format than expected",
+        };
+
+    try {
+        const credentialsRecord: Record<string, string> = {
+            username: credentials.username,
+            password: credentials.password,
+        };
+
+        const { data } = await axios.post(
+            baseUrl,
+            new URLSearchParams(credentialsRecord)
+        );
+        if (!data)
+            return {
+                success: false,
+                error: "Invalid Credentials",
+            };
+
+        const { access_token, token_type } = data as Token;
+        const newSession: Session = {
+            username: credentials.username,
+            access_token,
+            token_type,
+        };
+        const sessionStored = storeSession(newSession);
+        if (!sessionStored.success)
+            return {
+                success: false,
+                error: sessionStored.error,
+            };
+
+        return {
+            success: true,
+            data: newSession,
+        };
+    } catch (e) {
+        const error = e as Error;
+        return {
+            success: false,
+            error: error.message,
+        };
+    }
 };
 
 export default { login };
